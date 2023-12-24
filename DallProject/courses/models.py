c5d68fece3975292a5603357537148ac6a701391
m7mdhusinn <m7md.husinn@gmail.com>
2023-12-24 12:02:32 +0300

    setting up

--- DallProject/courses/models.py
+++ DallProject/courses/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    duration= models.CharField(max_length=100)
+    provider= models.CharField(max_length=100)
+    
