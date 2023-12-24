4e01fc19f9cb0aee06c6f2e2112c5a779c8f2f23
m7mdhusinn <m7md.husinn@gmail.com>
2023-12-24 12:08:43 +0300

    update

--- DallProject/companies/models.py
+++ DallProject/companies/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.
-# class Copmany(models.Model):
-#     name = models.CharField(max_length=100)
-#     description = models.TextField()
-#     industry=
+#Create your models here.
+class Copmany(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    industry=models.CharField(max_length=100)
