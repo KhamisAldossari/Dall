f1129ae8ac08444dc1892f53e933f4ec395b9e5c
m7mdhusinn <m7md.husinn@gmail.com>
2023-12-24 13:19:08 +0300

    add models

--- DallProject/majors/models.py
+++ DallProject/majors/models.py
@@ -1,8 +1,16 @@
 from django.db import models
-
+from certificates.models import Certificate
+from companies.models import Copmany
+from courses.models import Course
+from jobs.models import Job
+from skills.models import Skill
 # Create your models here.
 class Major(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    jops= models.ManyToManyField(Jop)
+    jops= models.ManyToManyField(Job)
+    certificates=models.ManyToManyField(Certificate)
+    companies=models.ManyToManyField(Copmany)
+    courses=models.ManyToManyField(Course)
+    skills=models.ManyToManyField(Skill)
 
